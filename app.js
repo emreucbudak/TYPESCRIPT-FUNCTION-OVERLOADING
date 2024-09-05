26ea1d1592473b535ca47c27fe6465bda47711f9
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-09-05 21:09:08 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+function deneme(a, b) {
+    console.log(a + b);
+}
+deneme(16, 46);
+deneme('emre', 'üçbudak');
---
+++ app.ts
@@ -0,0 +1,7 @@
+function deneme (a:number,b:number):number
+function deneme (a:string,b:string):string
+function deneme (a:any,b:any):any {
+    console.log(a+b)
+}
+deneme(16,46)
+deneme('emre','üçbudak')
